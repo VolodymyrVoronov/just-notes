e357323f3ca47f16d0dedc6dd2a39886bc4f6229
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-05-20 16:21:19 +0200

    chore(components): created and styled LogoBig.

---
+++ components/LogoBig/LogoBig.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { motion } from "framer-motion";
+
+import styles from "./LogoBig.module.css";
+
+interface ILogoBigProps {
+  logoText?: string;
+}
+
+const LogoBig = ({ logoText = "Just Notes" }: ILogoBigProps): JSX.Element => {
+  return (
+    <motion.div
+      className={styles.root}
+      initial={{
+        x: -200,
+        opacity: 0,
+      }}
+      animate={{
+        x: 0,
+        opacity: 1,
+        transition: {
+          delay: 0.05,
+          duration: 1,
+        },
+      }}
+    >
+      {logoText.split(" ").map((word) => (
+        <div key={uuidv4()} className={styles.word}>
+          {word.split("").map((letter) => (
+            <span key={uuidv4()} className={styles.letter}>
+              {letter}
+            </span>
+          ))}
+        </div>
+      ))}
+    </motion.div>
+  );
+};
+
+export default LogoBig;
--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,12 +1,16 @@
 import { NextPage } from "next";
 import React from "react";
 
+import LogoBig from "../../../components/LogoBig/LogoBig";
+
 import styles from "./styles.module.css";
 
 const StartPage: NextPage = (): JSX.Element => {
   return (
     <div className={styles.root}>
-      <div className={styles["left-side"]}>Left</div>
+      <div className={styles["left-side"]}>
+        <LogoBig />
+      </div>
       <div className={styles["right-side"]}>Right</div>
     </div>
   );
