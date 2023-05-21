33a4b3d775eae8281c121a069f911dcaee2e01de
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-05-21 15:27:29 +0200

    chore(app): updated start page.

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -2,6 +2,7 @@
 
 import { NextPage } from "next";
 import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 import { FormType, TForm } from "../../../types/form-variants";
 
@@ -32,11 +33,69 @@ const StartPage: NextPage = (): JSX.Element => {
           rightButton="Signup"
         />
 
-        {toggleFormType === FormType.signin ? (
-          <SigninForm className={styles["signin-form"]} />
-        ) : (
-          "Signup"
-        )}
+        <motion.div
+          initial={{
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            transition: {
+              delay: 0.05,
+              duration: 1,
+              ease: [0, 0.71, 0.2, 1.01],
+              scale: {
+                type: "spring",
+                damping: 5,
+                stiffness: 100,
+                restDelta: 0.001,
+              },
+            },
+          }}
+        >
+          <AnimatePresence mode="wait">
+            {toggleFormType === FormType.signin ? (
+              <motion.div
+                key={toggleFormType}
+                initial={{
+                  opacity: 0,
+                }}
+                exit={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                  transition: {
+                    duration: 0.5,
+                    ease: "easeInOut",
+                  },
+                }}
+              >
+                <SigninForm className={styles["signin-form"]} />
+              </motion.div>
+            ) : (
+              <motion.div
+                key={toggleFormType}
+                initial={{
+                  opacity: 0,
+                }}
+                exit={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                  transition: {
+                    duration: 0.5,
+                    ease: "easeInOut",
+                  },
+                }}
+              >
+                Signup
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </motion.div>
       </div>
     </div>
   );
