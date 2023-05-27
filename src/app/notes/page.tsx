c4e1ca4e5a3b65df254af749a2c65d6dc818e291
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-05-27 15:11:42 +0200

    chore(app): updated page notes.

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -2,15 +2,38 @@
 
 import { getServerSession } from "next-auth";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { motion, AnimatePresence } from "framer-motion";
 
 import { authOptions } from "../api/auth/[...nextauth]/route";
+
+import styles from "./styles.module.css";
 
 const NotesPage = () => {
   const session = useSession();
 
   console.log("session", session);
 
-  return <div>NotesPage</div>;
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      exit={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+        transition: {
+          duration: 0.5,
+          ease: "easeInOut",
+        },
+      }}
+      className={styles.root}
+    >
+      <div className={styles["left-side"]}>Left</div>
+      <div className={styles["right-side"]}>Right</div>
+    </motion.div>
+  );
 };
 
 export default NotesPage;
